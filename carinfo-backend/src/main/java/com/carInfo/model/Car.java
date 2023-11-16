585c279f7f041772f5991b63a8bf64af8af8c126
jaoosth022 <jaoosth022@student.wethinkcode.co.za>
2023-11-16 13:29:30 +0200

    added Car model and repository

---
+++ carinfo-backend/src/main/java/com/carInfo/model/Car.java
@@ -0,0 +1,83 @@
+package com.carInfo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String model;
+    private String make;
+    private int year;
+
+    public Car() {
+        // Default constructor required by JPA
+    }
+
+    public Car(String name, String model, String make, int year) {
+        this.name = name;
+        this.model = model;
+        this.make = make;
+        this.year = year;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                ", make='" + make + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
